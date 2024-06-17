b1d269a113f1c841fa52799de508b62cdf591e79
Jacob <techsupport@kreindler.ca>
2024-06-17 18:08:42 -0400

    auto colour when scrolled to on mobile

---
+++ src/js/mobileHover.js
@@ -0,0 +1,20 @@
+// get the y position of the tile and if it is in the middle of the screen remove the css filter property
+
+// Check if it's a mobile device
+const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+if (isMobile) {
+  window.addEventListener('scroll', () => {
+    const tiles = document.querySelectorAll('.tile');
+    tiles.forEach((tile) => {
+      const rect = tile.getBoundingClientRect();
+      const y = rect.top;
+      console.log(y);
+      if (y < window.innerHeight / 2 && y > -window.innerHeight / 2) {
+        tile.style.filter = 'none';
+      } else {
+        tile.style.filter = 'grayscale(100%)';
+      }
+    });
+  });
+}
