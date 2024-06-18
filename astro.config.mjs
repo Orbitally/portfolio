04339a3617159f986b72b07da67c89b0896c16bc
Jacob <techsupport@kreindler.ca>
2024-06-18 16:16:29 -0400

    added close button to project frame

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,28 +3,33 @@ import vercel from '@astrojs/vercel/serverless';
 import icon from 'astro-icon';
 import sitemap from '@astrojs/sitemap';
 
-import robotsTxt from "astro-robots-txt";
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   site: 'https://jacob.kreindler.ca',
   redirects: {
-    '/projects': '/'
+    '/projects': '/',
   },
   adapter: vercel({
     webAnalytics: {
-      enabled: true
+      enabled: true,
     },
-    maxDuration: 8
+    maxDuration: 8,
   }),
   images: {
-    domains: ['images.ctfassets.net', 'cdn.contentful.com']
+    domains: ['images.ctfassets.net', 'cdn.contentful.com'],
   },
-  integrations: [icon({
-    include: {
-      mdi: ['github'],
-      carbon: ['email']
-    }
-  }), sitemap(), robotsTxt()]
+  integrations: [
+    icon({
+      include: {
+        mdi: ['github'],
+        carbon: ['email', 'close-outline'],
+        solar: ['close-circle-bold'],
+      },
+    }),
+    sitemap(),
+    robotsTxt(),
+  ],
 });
--- src/js/projectFrame.js
+++ src/js/projectFrame.js
@@ -3,6 +3,8 @@ const frame = document.getElementById('frame');
 // Select all the elements with the class "tile" and add an event listener to each one
 const tiles = document.querySelectorAll('.tile');
 
+const closeBtn = document.getElementById('closeFrame');
+
 function closeFrame() {
   // unlock background scrolling
   document.documentElement.style.overflow = 'auto';
@@ -10,6 +12,20 @@ function closeFrame() {
   setTimeout(() => {
     frame.style.display = 'none';
     frame.classList.remove('slide-out-bck-center');
+  }, 400);
+}
+
+function closeFrameByBtn() {
+  // unlock background scrolling
+  document.documentElement.style.overflow = 'auto';
+  window.parent.document
+    .getElementById('frame')
+    .classList.add('slide-out-bck-center');
+  setTimeout(() => {
+    window.parent.document.getElementById('frame').style.display = 'none';
+    window.parent.document
+      .getElementById('frame')
+      .classList.remove('slide-out-bck-center');
   }, 400);
 }
 
@@ -31,16 +47,11 @@ tiles.forEach((tile) => {
   });
 });
 
-// If the user clicks outside the frame, close the frame
-window.addEventListener('click', (event) => {
-  if (event.target === frame) {
-    closeFrame();
-  }
-});
-
 // If the user presses the escape key, close the frame
 window.addEventListener('keydown', (event) => {
   if (event.key === 'Escape') {
     closeFrame();
   }
 });
+
+closeBtn.addEventListener('click', closeFrameByBtn);
