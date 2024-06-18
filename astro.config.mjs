2d524cf9f569734448ddcb87166c1283a6f3caef
Jacob <techsupport@kreindler.ca>
2024-06-17 23:46:18 -0400

    added `astro-sitrmap` and `astro-robots-txt` integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,30 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
-
 import icon from 'astro-icon';
+import sitemap from '@astrojs/sitemap';
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  site: 'https://jacob.kreindler.ca',
   redirects: {
-    '/projects': '/',
+    '/projects': '/'
   },
   adapter: vercel({
     webAnalytics: {
-      enabled: true,
+      enabled: true
     },
-    maxDuration: 8,
+    maxDuration: 8
   }),
   images: {
-    domains: ['images.ctfassets.net', 'cdn.contentful.com'],
+    domains: ['images.ctfassets.net', 'cdn.contentful.com']
   },
-  integrations: [
-    icon({
-      include: {
-        mdi: ['github'],
-        carbon: ['email'],
-      },
-    }),
-  ],
+  integrations: [icon({
+    include: {
+      mdi: ['github'],
+      carbon: ['email']
+    }
+  }), sitemap(), robotsTxt()]
 });
